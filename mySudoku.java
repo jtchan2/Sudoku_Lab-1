b2275531659609d20febd029f0f461dd3f9df1eb
jtchan2 <61221355+jtchan2@users.noreply.github.com>
2020-09-02 16:31:40 -0700

    Add files via upload

--- mySudoku.java
+++ mySudoku.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class mySudoku{
+	//checks if row doesnt have the number (run time ~O(n))
 	public  boolean isRowClean(int [][]board, int row, int num){
 		for(int i=0; i<9; i++){
 			if(board[row][i]==num){
@@ -8,6 +9,7 @@ public class mySudoku{
 		}
 		return true;
 	}
+	//O(n)
 	public  boolean isColClean(int [][]board, int col, int num){
 		for(int i=0; i<9; i++){
 			if(board[i][col]==num){
@@ -16,7 +18,7 @@ public class mySudoku{
 		}
 		return true;
 	}
-	//
+	//Checks if in a 3 by 3 square if the number is contained in it.
 	public boolean is3x3Clean(int [][]board, int row, int col, int num){
 		for(int i=0; i<3; i++){
 			for(int j=0; j<3; j++){
@@ -29,6 +31,7 @@ public class mySudoku{
 	}
 	//function isSpaceEmpty checks if the grid location has no numerical value
 	//So that it can place a number in that slot
+	//O(n^2)
 	public int [] isSpaceEmpty(int [][]board){
 		int []emptySpace= new int[2];
 		emptySpace[0]=10;
@@ -46,14 +49,16 @@ public class mySudoku{
 		emptySpace[1]=10;
 		return emptySpace;
 	}
+	//This checks if the coordinate on the board cna place the specific number down
 	public boolean canPlace(int [][] board, int row, int col, int num){
-		if(isRowClean(board, row, num)==true&&isColClean(board, col, num)==true&&is3x3Clean(board, row-(row%3), col-(col%3), num)==true){
+		if(isRowClean(board, row, num)==true&&isColClean(board, col, num)==true &&is3x3Clean(board, row-(row%3), col-(col%3), num)==true){
 			return true;
 		}
 		return false;
 	}
-	// the Sudoku function absically runs sudoku where the method is a boolean return type such that if the puzzle is solvable, it will return a true if not it will return false
+	// the Sudoku function basically runs sudoku where the method is a boolean return type such that if the puzzle is solvable, it will return a true if not it will return false
 	//Back tracking in this part is where the code when a number cant be filled, it reverts the number back to a zero and goes back to the for loop to loop through the other numbers tha could be used
+	//
 	public boolean sudoku(int [][] board){
 		//int row;
 		//int col;
@@ -72,12 +77,16 @@ public class mySudoku{
 					return true;
 				}
 				//if number can not be placed in position, the previous slots nubmer is reverted back to zero
+				//aka backtracking
 				board[row][col]=0;
 			}
 		}
 		//it restarts nubmer elemination process for filling in the slot 
 		return false;
 	}
+	// this fucntion basiclly prints out the screen/board of sudoku
+	// where columns are made when j=2 or 5
+	//It also creates a line divission after every 3 rows
 	public void printScreen(int [][] board){
 		for(int i=0; i<9; i++){
 				for(int j=0; j<9; j++){
@@ -93,17 +102,15 @@ public class mySudoku{
 					
 				}
 				if(i==2|| i==5){
-					//System.out.println();
+					
 					System.out.println("-----------");
-					//System.out.println();
+					
 				}
 			}
 	}
 
 	public static void main(String [] args){
 		int [][] board= new int[9][9];
-		//int [][] board= new int[][] {{5,3,0,0,7,0,0,0,0},
-							//{6,0,0,1,9,5,0,0,0}, {0,9,8,0,0,0,0,6,0},{8,0,0,0,6,0,0,0,3},{4,0,0,8,0,3,0,0,1},{7,0,0,0,2,0,0,0,6},{0,6,0,0,0,0,2,8,0},{0,0,0,4,1,9,0,0,5},{0,0,0,0,8,0,0,7,9}};
 		Scanner input= new Scanner(System.in);
 		System.out.println("Sudoku: The CODE");
 		System.out.println("Enter your own Sudoku puzzle!");
@@ -112,6 +119,7 @@ public class mySudoku{
 				System.out.println("Enter a digits 1-9 and a 0 if you want the an empty space.");
 				System.out.println("What number for Row: "+(row+1)+" Column: "+(col+1));
 				int number= input.nextInt();
+				//While loop jsut cheecks if number inputed was a acceptable answer if not re runs until number is accepted 
 				while(number>9||number<0){
 					System.out.println("Sorry only digits 1 to 9 and 0 for indicating blank spaces!");
 					number=input.nextInt();
@@ -120,14 +128,15 @@ public class mySudoku{
 			}
 		}
 		
-
-		//This prints out the board
 		
 		mySudoku puzzle= new mySudoku();
 		puzzle.printScreen(board);
+		System.out.println();
+		//Checks if sudoku methods runs the whole way through aka puzzle solved, if not returns a false meaning not solvable 
 		if(puzzle.sudoku(board)==true){
-			System.out.println("puzzle is solved");
+			System.out.println("puzzle is solved:");
 			// correct visual of printing board
+			System.out.println();
 			puzzle.printScreen(board);
 		}else{
 			System.out.println("No answer");
